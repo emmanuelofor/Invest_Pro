29eae4e5de81ce5706571bd0f70c2e29c262349e
emmanuelofor <emmanuel.ofori@kibo.school>
2023-09-03 20:03:45 +0000

    created routes, templates and css

--- app.py
+++ app.py
@@ -1,19 +1,142 @@
 from flask import Flask, request, jsonify, render_template, redirect
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required
 from flask_migrate import Migrate
-from models import db, Investment, UserPortfolio, Resource  # Import db and models
+from models import db, Investment, UserPortfolio, Resource, User  
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_mail import Mail, Message
+from itsdangerous import URLSafeTimedSerializer
 
 app = Flask(__name__)
 
 # Database Configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///investpro.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your-secret-key'
 
 db.init_app(app)  # Initialize the database
 migrate = Migrate(app, db)  # Apply Migrations
 
+# Initialize Flask-Mail
+mail = Mail(app)
+app.config['MAIL_SERVER'] = 'smtp.your-email-provider.com'
+app.config['MAIL_USERNAME'] = 'your-email@example.com'
+app.config['MAIL_PASSWORD'] = 'your-email-password'
+
+# Serializer for generating random tokens
+s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 @app.route('/')
 def home():
-    return "Welcome to InvestPro!"
+    return render_template('index.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        # Create the user
+        user = User(username=username, email=email)
+        user.set_password(password)
+        
+        # Commit the user to the database
+        db.session.add(user)
+        db.session.commit()
+
+        # Generate token and send email for email confirmation
+        token = s.dumps(email, salt='email-confirm')
+        msg = Message('Confirm Email', sender='noreply@investpro.com', recipients=[email])
+        msg.body = f'Click here to confirm your email: http://127.0.0.1:5000/confirm_email/{token}'
+        mail.send(msg)
+
+        return 'Signup successful! Please check your email to confirm.'
+    return render_template('signup.html')
+
+@app.route('/confirm_email/<token>')
+def confirm_email(token):
+    try:
+        email = s.loads(token, salt='email-confirm', max_age=3600)
+    except:
+        return 'The confirmation link is invalid or has expired.'
+
+    user = User.query.filter_by(email=email).first()
+    if user:
+        user.email_confirmed = True
+        db.session.commit()
+    return 'Email confirmed!'
+
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        user = User.query.filter_by(email=email).first()
+        
+        if user:
+            # Generate a token
+            token = s.dumps(email, salt='reset-password')
+            
+            # Send an email
+            msg = Message('Password Reset Request', sender='noreply@investpro.com', recipients=[email])
+            msg.body = f'Reset your password by clicking this link: http://127.0.0.1:5000/reset_password/{token}'
+            mail.send(msg)
+            
+            return 'An email for password reset has been sent.'
+        
+        else:
+            return 'Email not found.'
+        
+    return render_template('reset_password_request.html')
+
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    try:
+        email = s.loads(token, salt='reset-password', max_age=3600)
+    except:
+        return 'The reset password link is invalid or has expired.'
+    
+    user = User.query.filter_by(email=email).first()
+    
+    if request.method == 'POST' and user:
+        new_password = request.form.get('new_password')
+        user.set_password(new_password)
+        db.session.commit()
+        
+        return 'Password has been updated!'
+        
+    return render_template('reset_password.html')
+
+
+# New route for login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for('home'))
+        flash('Invalid username or password')
+    return render_template('login.html')
+
+# New route for logout
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 @app.route('/add_investment', methods=['GET', 'POST'])
 def add_investment():
@@ -73,3 +196,4 @@ def list_resources():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
